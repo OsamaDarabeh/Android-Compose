e842d46e591c8dabf3b53c90efd558f1ea025063
Osama Darabeh <os.dar.93@gmail.com>
2021-02-09 13:46:17 +0300

    Material design

--- app/src/main/java/com/osdar/androidcompose/MainActivity.kt
+++ app/src/main/java/com/osdar/androidcompose/MainActivity.kt
@@ -3,20 +3,25 @@ package com.osdar.androidcompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                MyScreenContent()
+                MyLayoutContent()
             }
         }
     }
@@ -37,13 +42,44 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun PreviewFun() {
         MyApp {
-            MyScreenContent()
+            MyLayoutContent()
         }
     }
 
     @Composable
     fun MyApp(content: @Composable () -> Unit) {
-        MyScreenContent()
+        MyLayoutContent()
+    }
+
+    @Composable
+    fun MyLayoutContent(modifier: Modifier = Modifier) {
+        Row(
+            modifier
+                .padding(8.dp)
+                .clip(RoundedCornerShape(4.dp))
+//                .background(MaterialTheme.colors.surface)
+                .clickable(onClick = { })
+                .padding(16.dp)
+        ) {
+            Surface(
+                modifier = Modifier.preferredSize(50.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colors.surface.copy(alpha = 0.2f)
+            ) {
+
+            }
+
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = "Osama Darabeh", fontWeight = FontWeight.Bold)
+                Providers(AmbientContentAlpha provides ContentAlpha.medium) {
+                    Text(text = "3 min ago", style = MaterialTheme.typography.body2)
+                }
+            }
+        }
     }
 
     @Composable
@@ -59,7 +95,7 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun NameList(names: List<String>, modifier: Modifier = Modifier) {
         LazyColumn(modifier = modifier) {
-            items(names) { name ->
+            itemsIndexed(names) { _, name ->
                 run {
                     Greeting(name)
                     Divider(color = Color.Black)
@@ -69,7 +105,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     @Composable
-    private fun MyScreenContent(names: List<String> = listOf("Osama", "Darabeh","Darabeh","Darabeh")) {
+    private fun MyScreenContent(
+        names: List<String> = listOf(
+            "Osama",
+            "Darabeh",
+            "Darabeh",
+            "Darabeh"
+        )
+    ) {
         val time = remember { mutableStateOf(0) }
         AndroidComposeTheme() {
             Surface(color = Color.Yellow) {
@@ -87,14 +130,18 @@ class MainActivity : AppCompatActivity() {
     fun Greeting(text: String) {
         val image = imageResource(id = R.drawable.header)
         val typography = MaterialTheme.typography
-
+        var isSelected = remember { mutableStateOf(false) }
+        val backgroundColor =
+            androidx.compose.animation.animateAsState(if (isSelected.value) Color.Red else Color.Transparent)
         Column(modifier = Modifier.padding(16.dp)) {
             val imageModifier = Modifier
                 .preferredHeight(180.dp)
                 .fillMaxWidth()
                 .clip(shape = RoundedCornerShape(4.dp))
             Image(
-                image, modifier = imageModifier, contentScale = ContentScale.Crop
+                image,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
             )
 
             Spacer(modifier = Modifier.preferredHeight(16.dp))
@@ -105,7 +152,11 @@ class MainActivity : AppCompatActivity() {
                         "sights I saw $text",
                 style = typography.h6,
                 maxLines = 2,
-                overflow = TextOverflow.Ellipsis
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .padding(2.dp)
+                    .background(color = backgroundColor.value)
+                    .clickable(onClick = { isSelected.value = (!isSelected.value) })
             )
             Text(text = "Hello $text", style = typography.body2)
             Text(text = "Hello $text", style = typography.body2)
